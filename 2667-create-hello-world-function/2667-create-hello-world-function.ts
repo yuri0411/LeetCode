a0862a02c6217cca2e74fadeaa98438b9493da19
강유리 <yu_ri0411@naver.com>
2024-07-13 15:16:38 +0900

    Time: 52 ms (73.34%), Space: 49.7 MB (77.45%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.ts
@@ -0,0 +1,12 @@
+function createHelloWorld() {
+
+    return function (...args): string {
+        return "Hello World"
+    };
+};
+
+
+//  const f = createHelloWorld();
+//  f(); // "Hello World"
+
+
