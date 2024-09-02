e9d4c3c485fd37ab05de9e9e3d7fdc6d5c7b4fd0
강유리 <yu_ri0411@naver.com>
2024-09-02 23:37:20 +0900

    Time: 138 ms (16.98%), Space: 62.8 MB (80.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,6 +1,5 @@
 function groupAnagrams(strs: string[]): string[][] {
     const wordsMap: {[key in string]: string[]} = {};
-    let answer: string[][] = []
 
     for (let str of strs) {
         const key = str.split("").sort((a, b) => a > b ? 1 : - 1).join('');
@@ -11,10 +10,6 @@ function groupAnagrams(strs: string[]): string[][] {
         }
 
     }
-
-    for(let word of Object.values(wordsMap)) {
-        answer.push(word)
-    }
-
-    return answer
+    
+    return Object.values(wordsMap)
 };
